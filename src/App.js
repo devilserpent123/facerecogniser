3a080ba929b5947948bc174a31b63d2e65181266
unknown <psbkkp777@gmail.com>
2020-08-10 16:33:21 +0530

    Hey there

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import Rank from './components/Rank/Rank.js';
+import Clarifai from 'clarifai'
+import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 
-function App() {
+
+import './App.css';
+const app = new Clarifai.App({
+ apiKey: '7ede7756b2e1439a807165f3c7a7f4ec'
+});
+
+
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state={
+			input:'',
+			imageUrl: ''
+		}
+
+
+	}
+	onInputChange=(event)=>{
+		  this.setState({imageUrl: this.state.input});
+
+	}
+
+	onButtonSubmit=()=>{
+	this.setState({imageUrl: this.state.input});
+	app.models.predict( Clarifai.FACE_DETECT_MODEL, "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+     
+    }
+  );
+	}
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+    <FaceRecognition imageUrl={this.state.imageUrl}/>
+
     </div>
   );
+ }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const FaceRecognition=({imageUrl})=>{
+return(
+	  <div  className='center'>
+     <img src={imageUrl} />
+    </div>
+ 
+	);
+}
+export default FaceRecognition;
